4441b928498c543c4ebde5d15b32c290c2e60873
Samuel Berston <samuelberston@gmail.com>
2024-09-25 21:38:30 -0700

    set up s3/dynamodb for remote backend with state locking

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   } 
+
+  backend "s3" {
+    bucket         = "goap-state-bucket"
+    key            = "global-application-orchestration/terraform.tfstate"  # This is the path within the bucket
+    region         = "us-east-1"
+    dynamodb_table = "goap-state-lock"
+    encrypt        = true
+  }
 }   
 
 provider "aws" {
